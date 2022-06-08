b3ba192b1667a4e84d3f8172924bf94a739ff77f
dangthanhtri <dangthanhtrispkt@gmail.com>
2022-06-08 17:42:52 +0700

    Update full

--- arm.py
+++ arm.py
@@ -1,163 +1,101 @@
 
 """
-	Original by  https://github.com/umarsear/ESP8266-Connected-MP3-Player#
-	Thank you umarsear for your great work!
-	The board uses the JQ8400 MP3 audio chip. It supports 8KHz to 48KHz sampling frequencies for MP3 and WAV file format
-	
-	Not all commands are mapped. 
-	
-	Command format
-	
-	Each command is 8 bytes
-	Pos	Descrption 	Bytes	Value
-	1 	Start		1		0xAA
-	2	command		1		refer manual
-	3	Lenght		1		len(data 1,....,data n)
-	4	data		1		see below
-	5	datan   	1		optional, refer manual
-	6	SM			1		checksum of lower byte of (from Start to  datan )
-
-    See more byte code here: https://wiki.dfrobot.com/Voice_Module_SKU__DFR0534
+    See more Arduino code here: https://www.youtube.com/watch?v=md4RQzFbGR0
 """
 from setting import *
 from utility import *
-from machine import UART
+from servo import *
+import math
+import time
 
-class JQ8400:
-    def __init__(self, port):
-        rx_pin = PORTS_DIGITAL[port][0]
-        tx_pin = PORTS_DIGITAL[port][1]
-        self.uart = UART(1, baudrate=9600, rx=rx_pin,tx=tx_pin)
-        self.port = port
-    
-    def split(self, num):
-        return num >> 8, num & 0xFF
-    
-    def get_SM(self, b):
-        message_length=len(b)
-        bit_sum=0X00
-        for i in range((message_length)):
-            bit_sum += b[i]
-        #print("bit_sum:",bit_sum)
-        #Sum check: It is the lower 8 bits of the sum of all the previous bytes, that is, the lower 8 bits are taken after the start code is added to the data
-        SM_Code=(0xAA  + bit_sum ) & 0xFF
-        return SM_Code
+class Arm():
 
-    # Khởi tạo mảng byte ban đầu
-    def command_base(self):
-        command=bytearray()
-        command.append(0xAA)##[0]
-        command.append(0x00)##[1]
-        command.append(0x00)##[2]
-        command.append(0x00)##[3]
-        return command
+    def __init__(self, base=0, right=1, left=2, gripper=3):
+        self.base = base
+        self.right = right
+        self.left = left
+        self.gripper = gripper
 
-    # Phát 1 bài nhạc biếách bài hát
-    def play_track(self, track_id):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x07
-        command[2]=0x02
-        HighByte, LowByte = self.split(track_id)
-        command[3]=HighByte
-        command.append(LowByte)
-        #print("HighByte: ",HighByte)
-        #print("LowByte: ",LowByte)
-        b=[command[1],command[2],command[3],command[4]]
-        SM_Code=self.get_SM(b) 
-        command.append(SM_Code)
-        self.uart.write(command)
+    def setup(self):
+        servo.position(self.base, 90)
+        servo.position(self.right, 90)
+        servo.position(self.left, 90)
+        servo.position(self.gripper, 90)
 
-    def play(self):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x02
-        command[3]=0xAC
-        self.uart.write(command)
-    
-    def pause(self):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x03
-        command[3]=0xAD
-        self.uart.write(command)
+        say('Arm setup done!')
 
-    def stop(self):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x04
-        command[3]=0xAE
-        self.uart.write(command)
+    # Servo Base (Theta)
+    def moveBase(self, moveToBase):
+        # limit min/max values
+        if moveToBase < 0:
+            moveToBase = 0
+        if moveToBase > 180:
+            moveToBase = 180
+        servo.position(self.base, moveToBase)
 
+    # Servo Gripper
+    def moveGripper(self, moveToGripper):
+        # limit min/max values
+        if moveToGripper  < 0:
+            moveToGripper  = 0
+        if moveToGripper  > 90:
+            moveToGripper  = 90
+        servo.position(self.gripper, moveToGripper)
 
-    def play_next(self):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x06
-        command[3]=0xB0
-        self.uart.write(command)
+    # Servo Right
+    def moveRight(self, moveToRight):
+        servo.release(self.left)
+        # limit min/max values
+        if moveToRight  < 50:
+            moveToRight  = 50
+        if moveToRight  > 179:
+            moveToRight  = 179
+        servo.position(self.right, moveToRight)
 
-    def play_previous(self):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x05
-        command[3]=0xAF
-        self.uart.write(command)
+    # Servo Left
+    def moveLeft(self, moveToLeft):
+        servo.release(self.right)
+        # limit min/max values
+        if moveToLeft  < 5:
+            moveToLeft  = 5
+        if moveToLeft  > 140:
+            moveToLeft  = 140
+        servo.position(self.left, moveToLeft)
 
-    def set_volume(self, level):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x13
-        command[2]=0x01
+    # Move the arm along the r axis (polar coordinates), or in height (z)
+    def moveRZ(self, moveToR=80, moveToZ=80):
+        # limit min/max values
+        if (moveToR < 20):
+            moveToR = 20
+        if (moveToZ < -25):
+            moveToZ = -25
+        if (moveToR > 130):
+            moveToR = 130
+        if (moveToZ > 125):
+            moveToZ = 125
+        
+        c = math.sqrt(moveToZ*moveToZ + moveToR*moveToR) # pythagorean theorem
+        K = math.atan2(moveToZ, moveToR)
+        
+        # fixed length bicep and forearm respectively
+        a = 81
+        b = 81
 
-        command[3]=level
-        b=[command[1],command[2],command[3]]
-        SM_Code=self.get_SM(b) 
-        command.append(SM_Code)
-        self.uart.write(command)
+        B = math.acos((a*a + c*c - b*b) / (2*a*c)) # cosine law
+        C = math.pi - 2*B # (180 - A - B) and A==B from isoceles
+        C = C * 180 / math.pi # rad --> deg
 
-    def volume_up(self):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x14
-        command[3]=0xBE
-        self.uart.write(command)
+        rightServoAngle = (K+B) * 180 / math.pi # servo 1 (right)
+        X = 90 - rightServoAngle # right angle subtraction
+        Y = 90 - X # sum of interior angles again
+        W = 180 - C - Y
+        leftServoAngle = W
 
-    def volume_down(self):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x15
-        command[3]=0xBF
-        self.uart.write(command)
+        if (math.isnan(leftServoAngle) != math.isnan(rightServoAngle)):
+            print('ERROR: Outside of boundaries!')
+            return
 
-    #Switch to flash card
-    #AA 0B 01 02 B8 switch to FLASH card, it will stop after switching
-    def use_flash(self):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x0B
-        command[2]=0x01
-        command[3]=0x02
-        command.append(0xB8) 
-        self.uart.write(command)
+        servo.position(self.right, 175-rightServoAngle) # see REPORT.pdf for explanation
+        servo.position(self.left, 90-leftServoAngle) # and diagrams of angle calculations
 
-    #Command: AA 16 03 drive letter track high track low SM
-    #return: none
-    #Definition of drive letter: stop after switching drive letter
-    #USB:00 SD:01 FLASH:02 NO_DEVICE: FF
-    #For example: AA 16 03 00 00 09 CC insert the 9th song in the U disk
-    #Description: Return to the insertion point to continue playing after the insertion ends
-    def insert_play(self, track_id):
-        command=bytearray()
-        command=self.command_base()
-        command[1]=0x16
-        command[2]=0x03
-        command[3]=0x02
-        HighByte, LowByte = self.split(track_id)
-        command.append(HighByte)
-        command.append(LowByte)
-        b=[command[1],command[2],command[3],command[4],command[5]]
-        SM_Code=self.get_SM(b)
-        command.append(SM_Code)
-        self.uart.write(command)
-
+arm = Arm()
--- definition.js
+++ definition.js
@@ -1,161 +1,295 @@
-Blockly.Blocks['xbot_sound_start'] = {
+Blockly.Blocks['arm_init'] = {
   init: function() {
     this.jsonInit(
       {
-        type: "xbot_sound_start",
-        message0: "khởi động máy nghe nhạc cổng %1",
+        type: "arm_init",
+        message0: "thiết lập servo giữa %1 phải %2 trái %3 đầu gắp %4",
         previousStatement: null,
         nextStatement: null,
         args0: [
           {
             type: "field_dropdown",
-            name: "port",
+            name: "base",
             options: [
-              ["1", "0"],
-              ["2", "1"],
-              ["3", "2"],
-              ["4", "3"],
-              ["5", "4"],
-              ["6", "5"],
+              ["S1", "0"],
+              ["S2", "1"],
+              ["S3", "2"],
+              ["S4", "3"],
+              ["S5", "4"],
+              ["S6", "5"],
+              ["S7", "6"],
+              ["S8", "7"],
             ],
-          },],
-        colour: "#00A06B",
+          },
+          {
+            type: "field_dropdown",
+            name: "right",
+            options: [
+              ["S2", "1"],
+              ["S1", "0"],
+              ["S3", "2"],
+              ["S4", "3"],
+              ["S5", "4"],
+              ["S6", "5"],
+              ["S7", "6"],
+              ["S8", "7"],
+            ],
+          },
+          {
+            type: "field_dropdown",
+            name: "left",
+            options: [
+              ["S3", "2"],
+              ["S1", "0"],
+              ["S2", "1"],
+              ["S4", "3"],
+              ["S5", "4"],
+              ["S6", "5"],
+              ["S7", "6"],
+              ["S8", "7"],
+            ],
+          },
+          {
+            type: "field_dropdown",
+            name: "gripper",
+            options: [
+              ["S4", "3"],
+              ["S1", "0"],
+              ["S2", "1"],
+              ["S3", "2"],
+              ["S5", "4"],
+              ["S6", "5"],
+              ["S7", "6"],
+              ["S8", "7"],
+            ],
+          },
+        ],
+        colour: "#154c79",
         tooltip: "",
         helpUrl: ""
       }
-    );
+    )
   },
   getDeveloperVars: function() {
-    return ['sound'];
+    return ['arm'];
   }
 };
 
-Blockly.Blocks['xbot_sound_action'] = {
+Blockly.Blocks['arm_setup'] = {
   init: function() {
     this.jsonInit(
       {
-        type: "xbot_sound_action",
-        message0: "%1",
+        type: "arm_setup",
+        message0: "về tọa độ gốc",
+        previousStatement: null,
+        nextStatement: null,
+        args0: [
+        ],
+        colour: "#154c79",
+        tooltip: "",
+        helpUrl: ""
+      }
+    )
+  },
+  getDeveloperVars: function() {
+    return ['arm'];
+  }
+};
+
+Blockly.Blocks['arm_moveGrip'] = {
+  init: function() {
+    this.jsonInit(
+      {
+        type: "arm_moveGrip",
+        message0: "%1 đầu gắp",
+        previousStatement: null,
+        nextStatement: null,
         args0: [
           {
             type: "field_dropdown",
             name: "action",
             options: [
-              [
-                "Phát nhạc",
-                "play"
-              ],
-              [
-                "Tạm dừng",
-                "pause"
-              ],
-              [
-                "Dừng phát nhạc",
-                "stop"
-              ],
-              [
-                "Phát bài nhạc kế tiếp",
-                "play_next"
-              ],
-              [
-                "Phát bài nhạc trước đó",
-                "play_previous"
-              ]
-            ]
+              ["Đóng", "90"],
+              ["Mở", "0"]
+            ],
           }
         ],
-        previousStatement: null,
-        nextStatement: null,
-        colour: "#00A06B",
+        colour: "#154c79",
         tooltip: "",
         helpUrl: ""
       }
-    );
+    )
   },
   getDeveloperVars: function() {
-    return ['sound'];
+    return ['arm'];
   }
 };
 
-
-Blockly.Blocks['xbot_sound_vol'] = {
+Blockly.Blocks['arm_moveBase'] = {
   init: function() {
     this.jsonInit(
       {
-        type: "xbot_sound_vol",
-        message0: "mở âm lượng %1 (0-30)",
-        args0: [
+        "type": "arm_moveBase",
+        "message0": "xoay servo chính giữa góc %1 (0-180)",
+        "args0": [
           {
-            type: "input_value",
-            name: "vol"
+            "type": "input_value",
+            "name": "angle",
+            "check": "Number"
           }
         ],
-        previousStatement: null,
-        nextStatement: null,
-        colour: "#00A06B",
-        tooltip: "",
-        helpUrl: ""
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#154c79",
+        "tooltip": "",
+        "helpUrl": ""
       }
     );
   },
   getDeveloperVars: function() {
-    return ['sound'];
+    return ['arm'];
   }
 };
 
-Blockly.Blocks['xbot_sound_playtrack'] = {
+Blockly.Blocks['arm_moveRight'] = {
   init: function() {
     this.jsonInit(
       {
-        type: "xbot_sound_playtrack",
-        message0: "phát bài nhạc số %1",
-        args0: [
+        "type": "arm_moveRight",
+        "message0": "xoay servo bên phải góc %1 (0-180)",
+        "args0": [
           {
-            type: "input_value",
-            name: "track"
+            "type": "input_value",
+            "name": "angle",
+            "check": "Number"
           }
         ],
-        previousStatement: null,
-        nextStatement: null,
-        colour: "#00A06B",
-        tooltip: "",
-        helpUrl: ""
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#154c79",
+        "tooltip": "",
+        "helpUrl": ""
       }
     );
   },
   getDeveloperVars: function() {
-    return ['sound'];
+    return ['arm'];
+  }
+};
+
+Blockly.Blocks['arm_moveLeft'] = {
+  init: function() {
+    this.jsonInit(
+      {
+        "type": "arm_moveLeft",
+        "message0": "xoay servo bên trái góc %1 (0-180)",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "angle",
+            "check": "Number"
+          }
+        ],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#154c79",
+        "tooltip": "",
+        "helpUrl": ""
+      }
+    );
+  },
+  getDeveloperVars: function() {
+    return ['arm'];
+  }
+};
+
+Blockly.Blocks['arm_moveRZ'] = {
+  init: function() {
+    this.jsonInit(
+      {
+        "type": "arm_moveLeft",
+        "message0": "di chuyển đầu gắp tới vị trí R %1 Z %2",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "radius",
+            "check": "Number"
+          },
+          {
+            "type": "input_value",
+            "name": "height",
+            "check": "Number"
+          }
+        ],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#154c79",
+        "tooltip": "",
+        "helpUrl": ""
+      }
+    );
+  },
+  getDeveloperVars: function() {
+    return ['arm'];
   }
 };
 
 // Python Code
 
-Blockly.Python['xbot_sound_start'] = function(block) {
+Blockly.Python['arm_init'] = function(block) {
   // TODO: Assemble Python into code variable.
-  var port = block.getFieldValue('port');
-  Blockly.Python.definitions_['import_sound_player'] = 'from sound_player_jq8400 import JQ8400';
-  Blockly.Python.definitions_['create_sound'] = 'sound = JQ8400(' + port + ')';
+  var base = block.getFieldValue('base');
+  var right = block.getFieldValue('right');
+  var left = block.getFieldValue('left');
+  var gripper = block.getFieldValue('gripper');
+  Blockly.Python.definitions_['import_arm'] = 'from arm import *';
+  Blockly.Python.definitions_['create_arm'] = 'arm = Arm(' + base + ', ' + right + ', ' + left + ', ' + gripper + ')';
   var code = '';
   return code;
 };
 
-Blockly.Python['xbot_sound_action'] = function(block) {
-  var dropdown_action = block.getFieldValue('action');
+Blockly.Python['arm_setup'] = function(block) {
   // TODO: Assemble Python into code variable.
-  var code = 'sound.' + dropdown_action + '()\n';
+  var code = "arm.setup()\n";
   return code;
 };
 
-Blockly.Python['xbot_sound_vol'] = function(block) {
-  var number_vol = Blockly.Python.valueToCode(block, 'vol', Blockly.Python.ORDER_ATOMIC);
+Blockly.Python['arm_moveGrip'] = function(block) {
   // TODO: Assemble Python into code variable.
-  var code = 'sound.set_volume(' + number_vol + ')\n';
+  var action = block.getFieldValue('action');
+  var code = "arm.moveGripper(" + action + ")\n";
   return code;
 };
 
-Blockly.Python['xbot_sound_playtrack'] = function(block) {
-  var number_track = Blockly.Python.valueToCode(block, 'track', Blockly.Python.ORDER_ATOMIC);
+Blockly.Python["arm_moveBase"] = function (block) {
+  var angle = Blockly.Python.valueToCode(block, 'angle', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = 'sound.play_track(' + number_track + ')\n';
+  var code = "arm.moveBase(" + angle + ")\n";
+  return code;
+};
+
+Blockly.Python["arm_moveRight"] = function (block) {
+  var angle = Blockly.Python.valueToCode(block, 'angle', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = "arm.moveRight(" + angle + ")\n";
+  return code;
+};
+
+Blockly.Python["arm_moveLeft"] = function (block) {
+  var angle = Blockly.Python.valueToCode(block, 'angle', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = "arm.moveLeft(" + angle + ")\n";
+  return code;
+};
+
+Blockly.Python["arm_moveRZ"] = function (block) {
+  var radius = Blockly.Python.valueToCode(block, 'radius', Blockly.Python.ORDER_ATOMIC);
+  var height = Blockly.Python.valueToCode(block, 'height', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = "arm.moveRZ(" + radius + ', ' + height + ")\n";
   return code;
 };
